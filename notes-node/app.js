3c8d632d1ff8993c8027f65c4195b420eb05f08e
Ryoung27 <RRYoung89@gmail.com>
2019-01-14 15:02:50 -0600

    add note

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,13 +5,16 @@ const fs = require('fs');
 const os = require('os');
 const notes= require('./notes.js');
 
-let user = os.userInfo();
+let res = notes.addNote();
+console.log(res);
+
+// let user = os.userInfo();
 
 // console.log(user.username);
 
 // fs.appendFileSync('greetings.txt', 'Hello ' + user.username + "!");
 
-fs.appendFileSync('test.txt', `Hello ${user.username}. You are ${notes.age}!`);
+// fs.appendFileSync('test.txt', `Hello ${user.username}. You are ${notes.age}!`);
 // fs.appendFileSync('testapp.js', console.log("It worked!"));
 
 // node testapp.js;
