6a2a2ef5f8ba88c8c08c80f09b2896b7c85af23f
Ryoung27 <RRYoung89@gmail.com>
2019-01-15 10:19:57 -0600

    infinite loop

--- notes-node/playground/json.js
+++ notes-node/playground/json.js
@@ -6,7 +6,24 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 
-let personString = '{"name": "Richie","age": 29}';
-let person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+// let personString = '{"name": "Richie","age": 29}';
+// let person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+let originalNote = {
+    title: "Some title",
+    body: "Some body"
+}
+
+let originalNoteString = JSON.stringify(originalNote);
+console.log(typeof originalNoteString);
+console.log(originalNoteString);
+
+fs.appendFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+// fs.appendFileSync('greetings.txt', 'Hello ' + user.username + "!");
+console.log(typeof note);
+console.log(note.title);
