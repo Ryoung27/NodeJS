9e0cb7c882443ba863fd024dfba5a0ad5985283c
Ryoung27 <RRYoung89@gmail.com>
2019-01-15 08:27:28 -0600

    functionally adds

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,21 +1,29 @@
 console.log("Starting notes.js");
 
+let addNote = (title, body) => {
+    console.log('Adding note', title, body)
+};
+
+module.exports = {
+    addNote: addNote
+};
+
 // console.log(module);
 // module.exports.age = 28;
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New Note';
-}
+// module.exports.addNote = () => {
+//     console.log('addNote');
+//     return 'New Note';
+// }
 
-module.exports.addition = (x, y) => {
-    return x + y;
-}
+// module.exports.addition = (x, y) => {
+//     return x + y;
+// }
 
-module.exports.subtraction = (x, y) => {
-    return x - y;
-}
+// module.exports.subtraction = (x, y) => {
+//     return x - y;
+// }
 
-module.exports.division = (x, y) => {
-    return x / y;
-}
+// module.exports.division = (x, y) => {
+//     return x / y;
+// }
