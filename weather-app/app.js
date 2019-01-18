cc80051e2060fb7fcca5df96741d40c484e44d59
Ryoung27 <RRYoung89@gmail.com>
2019-01-18 09:48:51 -0600

    request not reqeust

---
+++ weather-app/app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=iHhgGZRvXgkFaYXTtz9G8SY6WfePes81&location=1301%20lombard%20street%20philadelphia',
+    json: true
+}, (error, response, body) =>{
+    console.log(body);
+});
