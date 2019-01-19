c0fe58660d7cf4e1785ac8fb9153f25319c3d95c
Ryoung27 <RRYoung89@gmail.com>
2019-01-19 09:29:31 -0600

    set up argv

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,6 +2,15 @@ const request = require('request');
 
 const yargs = require('yargs')
 
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: "address",
+        describe: "Address to fetch weather.",
+        string: true
+    }
+});
+
 request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=iHhgGZRvXgkFaYXTtz9G8SY6WfePes81&location=1301%20lombard%20street%20philadelphia',
     json: true
