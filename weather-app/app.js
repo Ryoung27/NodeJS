bef9f3ceaa5c9af90ab9ae11e16756f4b0128b7f
Ryoung27 <RRYoung89@gmail.com>
2019-01-19 09:08:23 -0600

    got it working for this api

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,8 +4,9 @@ request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=iHhgGZRvXgkFaYXTtz9G8SY6WfePes81&location=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) =>{
-    // console.log(body.results[0].locations[0].street);
-    // console.log(`Address: ${body.results[0].locations[0].street}`);
-    console.log(`Latitude: ${body.results[0].locations[0].latLng}`);
-    // console.log(`Longitude: ${body.results[0].getometry.location.lng}`);
+
+    console.log(`Address: ${body.results[0].locations[0].street}`);
+    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+
 });
