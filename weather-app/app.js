8810942be744e30a68fa40c048dee59e00f3e337
Ryoung27 <RRYoung89@gmail.com>
2019-01-19 08:44:10 -0600

    Response

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=iHhgGZRvXgkFaYXTtz9G8SY6WfePes81&location=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) =>{
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(JSON.stringify(response, undefined, 2));
 });
