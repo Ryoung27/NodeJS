3d802517c54d2c33436ede654bdc34ffa495a214
Ryoung27 <RRYoung89@gmail.com>
2019-01-19 09:07:10 -0600

    got street working for current api

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,8 @@ request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=iHhgGZRvXgkFaYXTtz9G8SY6WfePes81&location=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) =>{
-    console.log(JSON.stringify(response, undefined, 2));
+    // console.log(body.results[0].locations[0].street);
+    // console.log(`Address: ${body.results[0].locations[0].street}`);
+    console.log(`Latitude: ${body.results[0].locations[0].latLng}`);
+    // console.log(`Longitude: ${body.results[0].getometry.location.lng}`);
 });
