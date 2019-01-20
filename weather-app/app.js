34c5ac0d33c9f3b214a3db7144ea326b32b318c4
Ryoung27 <RRYoung89@gmail.com>
2019-01-20 10:39:15 -0600

    fixed bug caused by different api

--- weather-app/app.js
+++ weather-app/app.js
@@ -23,11 +23,13 @@ request({
     url: `http://www.mapquestapi.com/geocoding/v1/address?key=iHhgGZRvXgkFaYXTtz9G8SY6WfePes81&location=${encodedAddress}`,
     json: true
 }, (error, response, body) =>{
+
+    // console.log(body);
     if (error){
         console.log('Unable to connect to Google Servers.');
-    } else if ( === 0) { //This needs to be different due to different api.
+    } else if (body.results[0].locations[0].street === "") { //This was fixed for mapquest api.
         console.log('Unable to find that address.')
-    } else if () { //This needs to be fixed for current api.
+    } else if (body.results[0].locations[0].street) { //This needs to be fixed for current api.
         console.log(`Address: ${body.results[0].locations[0].street}`);
         console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
         console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
