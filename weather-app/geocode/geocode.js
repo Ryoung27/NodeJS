ffff6570068e7b34ced1066b64db0a5d465085fb
Ryoung27 <RRYoung89@gmail.com>
2019-01-21 15:25:42 -0600

    callbacks

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -16,11 +16,17 @@ let geocodeAddress = (address, callback) => {
             callback('Unable to connect to server');
             // console.log('Unable to connect to Google Servers.');
         } else if (body.results[0].locations[0].street === "") { //This was fixed for mapquest api.
-            console.log('Unable to find that address.')
+            callback('Unable to find that address.');
+            // console.log('Unable to find that address.')
         } else if (body.results[0].locations[0].street) { //This needs to be fixed for current api.
-            console.log(`Address: ${body.results[0].locations[0].street}`);
-            console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-            console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+            callback(undefined, {
+                address: body.results[0].locations[0].street,
+                latitude: body.results[0].locations[0].latLng.lat,
+                longitude: body.results[0].locations[0].latLng.lng
+            })
+            // console.log(`Address: ${body.results[0].locations[0].street}`);
+            // console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+            // console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
         }
     });
 }
