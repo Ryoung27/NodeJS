d9dcbb90f1047fbc21afae60909e492f1cca6a22
Ryoung27 <RRYoung89@gmail.com>
2019-01-23 14:01:14 -0600

    got tempature

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,37 @@
-const yargs = require('yargs')
-const geocode = require('./geocode/geocode')
+// const yargs = require('yargs')
+// const geocode = require('./geocode/geocode')
 
-const argv = yargs.options({
-    a: {
-        demand: true,
-        alias: "address",
-        describe: "Address to fetch weather.",
-        string: true
-    }
-}).help().alias('help', 'h').argv;
+// const argv = yargs.options({
+//     a: {
+//         demand: true,
+//         alias: "address",
+//         describe: "Address to fetch weather.",
+//         string: true
+//     }
+// }).help().alias('help', 'h').argv;
 
-// console.log(argv);
+// // console.log(argv);
 
-// encodeURIComponent('text here to incode');
-// decodeURIComponent('test%20here')
+// // encodeURIComponent('text here to incode');
+// // decodeURIComponent('test%20here')
 
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage){
-        console.log(errorMessage);
-    } else{
-        console.log(JSON.stringify(results, undefined, 2));
-    }
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage){
+//         console.log(errorMessage);
+//     } else{
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+//API Key 8691e730fc26412960eaba6814fdedbe
+// https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+// Forecast information address: https://api.darksky.net/forecast/8691e730fc26412960eaba6814fdedbe/38.9117,-94.67237
+
+const request = require('request')
+
+request({
+url:"https://api.darksky.net/forecast/8691e730fc26412960eaba6814fdedbe/38.9117,-94.67237",
+json: true}, (error, response, body) =>{
+    console.log(body.currently.temperature);
 });
