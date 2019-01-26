1cf3708ba8b979e7b2eb3ba48fcc0b5d17972b0c
Ryoung27 <RRYoung89@gmail.com>
2019-01-26 08:50:24 -0600

    more promises

--- weather-app/playground/promises.js
+++ weather-app/playground/promises.js
@@ -1,12 +1,30 @@
-let somePromise = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve('Hey. It worked!');
-        // reject('Unable to fullfil promise.');
-    }, 2500);
+let asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number'){
+                resolve(a + b);
+            } else {
+                reject('Arguments must be numbers');
+            }
+        }, 1500);
+    });
+};
+
+asyncAdd(1, 2).then((res) => {
+    console.log('Result: ', res);
+}, (errorMessage) => {
+    console.log(errorMessage);
 });
 
-somePromise.then((message) =>{
-    console.log('Success: ', message);
-}, (errorMessage) => {
-    console.log('Error: ', errorMessage);
-});
+// let somePromise = new Promise((resolve, reject) => {
+//     setTimeout(() =>{
+//         resolve('Hey. It worked!');
+//         // reject('Unable to fullfil promise.');
+//     }, 2500);
+// });
+
+// somePromise.then((message) =>{
+//     console.log('Success: ', message);
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// });
