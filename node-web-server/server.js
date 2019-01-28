cf97887a0153356bb2cdb9b7b5b76678bf59d3f5
Ryoung27 <RRYoung89@gmail.com>
2019-01-28 06:48:26 -0600

    static directory

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,8 +2,28 @@ const express = require('express');
 
 let app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) =>{
-    res.send('Hello Express!')
+    // res.send("<h1>Hello Express!</h1>")
+    res.send({
+        name: "Richie",
+        likes: [
+            'Running',
+            'yoga',
+            'Being active'
+        ]
+    })
 });
+
+app.get('/about', (req,res) =>{
+    res.send("<h1>We're in about.</h1>")
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: "Sorry baby, bad page design.",
+    })
+})
 
 app.listen(3000);
