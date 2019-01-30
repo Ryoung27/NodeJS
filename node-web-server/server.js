5b936f0ce8a0332dda41cb7ec3e66343914923c4
Ryoung27 <RRYoung89@gmail.com>
2019-01-30 07:06:41 -0600

    helpers

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -7,6 +7,10 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear()
+});
+
 app.get('/', (req, res) =>{
     res.render('home.hbs', {
         pageTitle: 'Home Page',
