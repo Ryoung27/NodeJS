689817e58a3d04ce6b72a0efefb14491a373b295
Ryoung27 <RRYoung89@gmail.com>
2019-01-31 06:55:38 -0600

    down website link

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -6,7 +6,6 @@ let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
     let now = new Date().toString();
@@ -20,9 +19,12 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) =>{
-    res.render('maintance.hbs')
-});
+// app.use((req, res, next) =>{
+//     res.render('maintance.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerHelper('getCurrentYear', () =>{
     return new Date().getFullYear()
