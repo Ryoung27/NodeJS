b81aad614782d4f841cacea31f6dd0ea54ce2e40
Ryoung27 <RRYoung89@gmail.com>
2019-02-01 05:41:07 -0600

    async users

---
+++ async/app-promises.js
@@ -0,0 +1,14 @@
+const users = [{
+    id: 1,
+    name: "Richie",
+    schoolId: 101
+}, {
+    id: 2,
+    name: "Kayla",
+    schoolId: 101
+}];
+const grades = [];
+
+const getUser = (id) =>{
+
+}
