108e11ef6f1f0fd9ad551efe09ed48338eb3c6f2
Ryoung27 <RRYoung89@gmail.com>
2019-02-02 08:26:59 -0600

    always a typo never a typa

--- async/currency-convert.js
+++ async/currency-convert.js
@@ -2,20 +2,23 @@
 
 const axios = require('axios')
 
-// USD, CAD, Amount
 
-//Convert first value to second value.
+const getExchangeRate = async (from, to) => {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=8124fecb74330df0ca1c234ee097280a')
+    const euro = 1 / response.data.rates[from];
+    const rate = euro * response.data.rates[to];
+    return rate;
+};
 
-// http://data.fixer.io/api/latest?access_key=8124fecb74330df0ca1c234ee097280a
-
-const getExchangeRate = (from, to) => {
-    return axios.get('http://data.fixer.io/api/latest?access_key=8124fecb74330df0ca1c234ee097280a').then((response) => {
-        const euro = 1 / response.data.rates[from];
-        const rate = euro * response.data.rates[to];
-        return rate;
-    })
+const getCountries = async (currencyCode) => {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+    return response.data.map((country) => country.name);
 };
 
 getExchangeRate('USD', 'CAD').then((rate) => {
     console.log(rate);
+});
+
+getCountries('EUR').then((countries) => {
+    console.log(countries)
 });
