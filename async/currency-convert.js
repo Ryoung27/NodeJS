6942ce4de1945edaa449ca8b648053e5b7aa3121
Ryoung27 <RRYoung89@gmail.com>
2019-02-02 08:05:22 -0600

    added currency converter

--- async/currency-convert.js
+++ async/currency-convert.js
@@ -1,3 +1,21 @@
+// import Axios from "axios";
+
+const axios = require('axios')
+
 // USD, CAD, Amount
 
 //Convert first value to second value.
+
+// http://data.fixer.io/api/latest?access_key=8124fecb74330df0ca1c234ee097280a
+
+const getExchangeRate = (from, to) => {
+    return axios.get('http://data.fixer.io/api/latest?access_key=8124fecb74330df0ca1c234ee097280a').then((response) => {
+        const euro = 1 / response.data.rates[from];
+        const rate = euro * response.data.rates[to];
+        return rate;
+    })
+};
+
+getExchangeRate('USD', 'CAD').then((rate) => {
+    console.log(rate);
+});
